b5618e8106cfb5b1e93a0f1f678f8319ca49cf2c
Denis Yorsh <denis_my_mail@yahoo.com>
2023-07-05 14:19:05 +0300

    Initial commit

---
+++ src/main/java/org/itstep/diploma/DiplomaItstepApplication.java
@@ -0,0 +1,13 @@
+package org.itstep.diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaItstepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaItstepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/itstep/diploma/DiplomaItstepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itstep.diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaItstepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
