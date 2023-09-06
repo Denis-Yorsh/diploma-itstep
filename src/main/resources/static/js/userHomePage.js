4b8eb90362250e2349f917d3214068e180d36d84
Denis Yorsh <denis_my_mail@yahoo.com>
2023-09-06 14:27:34 +0300

    refactoring js, v2.1.5

--- src/main/resources/static/js/userHomePage.js
+++ src/main/resources/static/js/userHomePage.js
@@ -16,36 +16,6 @@ $(() => {
         })
     })
 
-    $(".add-post").on("click", function (event) {
-        event.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: $(this).attr("href"),
-            data: {_csrf: $(".csrf").val()},
-            dataType: "text",
-            success: function (response) {
-                let data = response.split("<!--------------------------------------------------->")
-                $(".data").html(data[1]);
-                listenerAddPost()
-            },
-        })
-    })
-
-    $(".all-posts").on("click", function (event) {
-        event.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: $(this).attr("href"),
-            data: {_csrf: $(".csrf").val()},
-            dataType: "text",
-            success: function (response) {
-                let data = response.split("<!--------------------------------------------------->")
-                $(".data").html(data[1]);
-                listenerDeletePost()
-            }
-        })
-    })
-
     const listenerSettings = () => {
         $(".form-settings").on("submit", function (event) {
             event.preventDefault();
@@ -138,59 +108,4 @@ $(() => {
             /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
     };
-
-    const listenerDeletePost = () => {
-        $(".delete-post").on("click", function (event) {
-            event.preventDefault();
-            $.ajax({
-                type: "DELETE",
-                url: "/post/deletePost",
-                data: {
-                    id: $(this).attr("href"),
-                    _csrf: $(".csrf").val()
-                },
-                dataType: "text",
-                success: function (response) {
-                    let data = response.split("<!--------------------------------------------------->")
-                    $(".data").html(data[1]);
-                    listenerDeletePost()
-                },
-            })
-        })
-    }
-
-    const listenerAddPost = () => {
-        $("#dk-my-form").on("submit", function (event) {
-            event.preventDefault();
-            const textOfPost = $(".textOfPost").val()
-            const imageFile = $(".imageFile")
-            if (!$(".postTitle").val() || !$(imageFile).val() || !textOfPost) {
-                $(".validate").text("all filed must be no empty")
-                return
-            }
-            if ($(imageFile)[0].files[0].size >= 1048576) {
-                $(".validate").text("file is too big max size img files 1 048 576 bytes")
-                return
-            }
-            if (textOfPost.length > 10_000) {
-                $(".validate").text("text for the post is too big, max 10_000 characters")
-                return
-            }
-            let thatForm = $(this)
-            let formData = new FormData(thatForm.get(0));
-            $.ajax({
-                type: "POST",
-                url: $(this).attr("action"),
-                contentType: false,
-                processData: false,
-                data: formData,
-                success: function (response) {
-                    $(".validate").text(response)
-                    $(".postTitle").val("")
-                    $(".imageFile").val(null)
-                    $(".textOfPost").val("")
-                }
-            })
-        })
-    }
 })
